83ed7be2686ca71e803d70c260f6c9f96ab76927
Christoph Holtmann <christoph.holtmann@live.de>
2017-05-11 20:02:12 +0200

    MediaProgramming - ToDo update

--- AI/BogaprClient/src/Main.java
+++ AI/BogaprClient/src/Main.java
@@ -1,22 +1,50 @@
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
 import lenz.htw.bogapr.Move;
 import lenz.htw.bogapr.net.NetworkClient;
 
+// java -Djava.library.path=lib/native -jar bogapr.jar
+
 public class Main
 {
+    enum Player
+    {
+        One, Two, Three
+    }
+
+    class FieldConfiguration
+    {
+        public int X;
+        public int Y;
+        public int Stones;
+        public Player Player;
+
+        public FieldConfiguration(int x, int y, int stones, Player player)
+        {
+            X = x;
+            Y = y;
+            Stones = stones;
+            Player = player;
+        }
+    }
+
+    // x, y, player
+    FieldConfiguration[][] configuration = new FieldConfiguration[13][7];
+
     public static void main(String[] args)
     {
         try
         {
             NetworkClient networkClient = new NetworkClient(null, "CLIENT 0", ImageIO.read(new File("Content/logo.png")));
 
-            //networkClient.getExpectedNetworkLatencyInMilliseconds();
-            //networkClient.getTimeLimitInSeconds();
-            //networkClient.getMyPlayerNumber();
+            int latency = networkClient.getExpectedNetworkLatencyInMilliseconds();
+            int time = networkClient.getTimeLimitInSeconds();
+            int playerNumber = networkClient.getMyPlayerNumber();
 
             for (;;) {
                 Move receiveMove;
@@ -24,10 +52,61 @@ public class Main
                     //Zug in meine Brettrepräsentation einarbeiten
                 }
                 //berechne tollen Zug
-                //networkClient.sendMove(new Move(abc));
-                }
-        } catch (IOException e) {
+
+
+                networkClient.sendMove(new Move(1, 1, 0, 2));
+
+//                if(playerNumber == 0)
+//                {
+//                    networkClient.sendMove(new Move(0, 1, 1, 3));//5, 6, 2, 6));
+//                }
+//                else if(playerNumber == 1)
+//                {
+//                    networkClient.sendMove(new Move(0, 1, 1, 3));
+//                }
+//                else
+//                {
+//                    networkClient.sendMove(new Move(0, 1, 1, 3));
+//                }
+            }
+        }
+        catch (IOException e)
+        {
             throw new RuntimeException("", e);
         }
+    }
+
+    private int[][] GetRandomFieldOfPlayer(Player player)
+    {
+        //List<int[][]> possibleFields = new List<int[][]>();
+
+        for(int x = 0; x < 12; x++)
+            for(int y = 0; y < 6; y++)
+            {
+                if(configuration[x][y].Player == player)
+                {
+                    return int[x][y];
+                }
+            }
+
+        return null;
+    }
+
+    private void SetStartConfiguration()
+    {
+        // blue
+        configuration[10][6] = new FieldConfiguration(3, Player.One);
+        configuration[11][6] = new FieldConfiguration(3, Player.One);
+        configuration[10][5] = new FieldConfiguration(3, Player.One);
+
+        // red
+        configuration[0][1] = new FieldConfiguration(3, Player.Two);
+        configuration[1][1] = new FieldConfiguration(3, Player.Two);
+        configuration[2][1] = new FieldConfiguration(3, Player.Two);
+
+        // green
+        configuration[5][0] = new FieldConfiguration(3, Player.Three);
+        configuration[1][6] = new FieldConfiguration(3, Player.Three);
+        configuration[2][6] = new FieldConfiguration(3, Player.Three);
     }
 }
--- MediaProgramming/messenger-model/src/de/sb/messenger/persistence/Person.java
+++ MediaProgramming/messenger-model/src/de/sb/messenger/persistence/Person.java
@@ -1,11 +1,14 @@
 package de.sb.messenger.persistence;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.List;
 
 import javax.persistence.*;
 import javax.validation.*;
 import javax.validation.constraints.*;
 import javax.validation.executable.ValidateOnExecution;
+
+import javafx.scene.Group;
 
 @Entity
 public class Person extends BaseEntity {
@@ -15,6 +18,7 @@ public class Person extends BaseEntity {
 		USER
 	}
 	
+	//TODO einfacher
 	@Pattern(regexp ="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
 	        +"[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
 	        +"(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
@@ -55,8 +59,21 @@ public class Person extends BaseEntity {
 	@Column
 	private Person peopleOberserved;
 	
-	protected Person() {
-		
+	public Person(final String email, final Document avatar) {
+		this.name = new Name();
+		this.adress = new Adress();
+		this.group = Group.USER;
+		this.messageAuthored = Collections.emptySet();
+		this.peopleOberserving = new Person();
+		this.peopleOberserved = new Person();
+		this.passHash = new byte[0];
+		this.email = email;
+		this.avatar = avatar;
+	}
+	
+	protected Person()
+	{
+		this(null, null);
 	}
 	
 	public String getEmail() {
